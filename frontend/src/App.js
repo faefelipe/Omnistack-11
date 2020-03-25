6f88dbfa6561328448efe2c0b92aae3c1fb5fa51
faefelipe <fae.20@hotmail.com>
2020-03-25 10:24:42 -0300

    Implemented frontend page logon

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-import Header from './Header';
+import './global.css'
 
-function App() {
-  const [counter, setCounter ] = useState(0);
-  
-  function increment(){
-    setCounter(counter + 1);   
-  }
-  
+import Logon from './pages/logon';
+
+function App() {  
   return (
     <div>
-      <Header>Contador: {counter}</Header>
-      <button onClick={increment}>Incrementar</button>
+      <Logon />      
     </div>
   );
 }
